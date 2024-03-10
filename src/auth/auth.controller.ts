b4e862b497d7e5e767ddb5dc2f543836242292d3
Sebastian Guerrero <sebastian302123321@gmail.com>
2024-03-10 16:42:59 -0500

    first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
+import { log } from 'console';
+import { RegisterUserDto } from './dto/register-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  create(@Body() createAuthDto: CreateUserDto) {
+    return this.authService.create(createAuthDto);
+  }
+
+  @Post('/register')
+  register(@Body() RegisterUserDto: RegisterUserDto) {
+    return this.authService.register(RegisterUserDto);
+  }
+
+  @Post('/login')
+  login( @Body() loginDto: LoginDto) {
+    console.log(loginDto);
+    return this.authService.login(loginDto);
+  }
+
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,82 @@
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+
+import * as bcrypt from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt.payload';
+import { LoginResponse } from './interfaces/login-response';
+import { log } from 'console';
+import { RegisterUserDto } from './dto/register-user.dto';
+
+@Injectable()
+export class AuthService {
+
+
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService
+  ) { }
+
+  async create(CreateUserDto: CreateUserDto): Promise<User> {
+    try {
+      const { password, ...userData } = CreateUserDto;
+      const newUser = new this.userModel({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+      await newUser.save();
+
+      // Remove password from response
+      const { password: _, ...user } = newUser.toJSON();
+      return user;
+
+    } catch (error) {
+
+      if (error.code === 11000) {
+        throw new BadRequestException(`Email ${CreateUserDto.email} already exists`);
+      }
+      throw new InternalServerErrorException(`Error creating user: ${error}`);
+
+    }
+  }
+
+  async login(loginDto: LoginDto): Promise<LoginResponse> {
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) {
+      throw new BadRequestException(`User with email ${email} not found`);
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new BadRequestException('Wrong password');
+    }
+
+    const { password: _, ...result } = user.toJSON();
+    return {
+      user: result,
+      token: this.getJWT({ id: user.id })
+    }
+  }
+
+  async register(registerUserDto: RegisterUserDto): Promise<LoginResponse> {
+
+    const user = await this.create(registerUserDto);
+    return {
+      user,
+      token: this.getJWT({ id: user._id })
+    }
+  }
+
+  getJWT(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
+}
